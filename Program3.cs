dd80770430d5c6395d7918c03cf762816c3a96f4
elian169 <78943835+elian169@users.noreply.github.com>
2021-02-12 00:28:10 -0400

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, impar = 0, par = 0;
+            for (i = 100; i < 120; i++)
+                if (i % 2 == 0)
+                    Console.Write("{0}|", i);
+
+            Console.Write("\n\n");
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace o
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra;
+            for (letra = 'z'; letra >= 'a'; letra--)
+                Console.WriteLine("{0}", letra);
+        }
+    }
+}
