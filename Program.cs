63bd9eb4f8eefc0c8b0a3072d9293bf3b9ebd16a
elian169 <78943835+elian169@users.noreply.github.com>
2021-02-11 20:03:36 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine("los numeros son: " + n);
+                n = n + 1;
+                
+            }
+        }
+    }
+}
