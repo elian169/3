b9963e5a23e2adbc020a7b6432517b46e8b26d44
elian169 <78943835+elian169@users.noreply.github.com>
2021-02-12 00:12:05 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 
-
-namespace _2
+namespace _3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = 1;
-            while (n <= 10)
-            {
-                Console.WriteLine("los numeros son: " + n);
-                n = n + 1;
-                
-            }
+            int i, impar = 0, par = 0;
+            for (i = 100; i < 120; i++)
+                if (i % 2 == 0)
+                    Console.Write("{0}|", i);
+
+            Console.Write("\n\n");
+
+
+            Console.Read();
         }
     }
 }
